74988a7f86da36e7cdfee23a5f8ce865b6156f40
Chetan4458 <111554115+Chetan4458@users.noreply.github.com>
2024-11-18 15:34:00 +0530

    Your commit message

---
+++ python/test3.py
@@ -0,0 +1,19 @@
+groq_api_key = "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"
+url='https://www.google.com'
+def example_function():
+    print("This is a sample function")
+
+def another_function():
+    for i in range(10) : # Added colon
+        print(i)
+
+    if i > 5:  # Unused variable 'i'
+        print("i is greater than 5" ) # Added closing parenthesis
+    else:
+        print("i is less than or equal to 5")  
+
+example_function()
+#another_function(10)  
+
+# No logical error 
+result = 5  
